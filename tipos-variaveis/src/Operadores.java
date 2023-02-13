73bcaf2811effbfec1de82ee54f83abae27ba6ff
caren pontes <carenoliveirasilvapontes@gmail.com>
2023-02-13 11:10:35 -0300

    Exemplos de operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,33 @@
+public class Operadores {
+  public static void main (String[]args){
+    String nomeCompleto="caren"+"pontes";
+      System.out.print(nomeCompleto);
+
+      //string concatenação comm números 
+      String concatenacao="?";
+
+      concatenacao=1+1+1+"1";
+
+      System.out.print(concatenacao);
+
+      concatenacao=1+"1"+1+1;
+      // apartir no caracter de texto ele nao soma mais ele só faz
+      // a concatenação mesmo sendo números depois ,ele nao volta a somar 
+
+      System.out.print(concatenacao);
+
+      concatenacao=1+"1"+1+1+"1";
+
+      System.out.print(concatenacao);
+
+      concatenacao="1"+1+1+1;
+
+      System.out.print(concatenacao);
+
+      concatenacao="1"+(1+1+1);
+
+      System.out.print(concatenacao);
+
+      
+  }  
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,20 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // fortemente tipado
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+        System.out.print(numeroCurto2);
+        // variáveis
+        int numero = 1;
+        numero = 2;
+        System.out.print(numero);
+        // valor-nunca-podera-mudar? (constantes)
+        final double VALOR_DE_PI = 3.14;
+        System.out.print(VALOR_DE_PI);
+
+        // String
+        String nome = "caren PONTES";
+        System.out.print(nome);
     }
 }
